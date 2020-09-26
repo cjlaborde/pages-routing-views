67fb248372645e5d6c85f8592539563074f7a283
Christian <cjlaborde@gmail.com>
2020-09-26 15:37:52 -0400

    Validating Parameters

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -3,7 +3,16 @@
 </template>
 
 <script>
-export default {};
+export default {
+  validate(data) {
+    // console.log(data);
+    // return true;
+    /* Validates incomming id to only accept id 1 */
+    // return data.params.id == 1;
+    /* Validates incomming id to only accept numbers using regex */
+    return /^\d+$/.test(data.params.id);
+  },
+};
 </script>
 
 <style>
